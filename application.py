8af366c1977f414c12f4fdc4555b562949f739cd
Bradley Rowe <bcrsf27@gmail.com>
2023-10-28 20:54:42 -0700

    Application framework

---
+++ application.py
@@ -0,0 +1,11 @@
+"""Application -- Menu and Metronome Launch"""
+
+
+class Application:
+    """Load existing settings into menu and execute UI function"""
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        """Do the application"""
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 """Main entry point into metronome"""
 
+from application import Application
+
 
 def main():
     """Main function of script"""
+
+    # Instantiate the application
+    app = Application()
+
+    # Do the thing
+    app.run()
 
 
 if __name__ == "__main__":
